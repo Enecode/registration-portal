6f07b85800304dd9c37f8ddd0cd55ba83c0768e2
Jacob Isah <isahjacob0@gmail.com>
2024-04-17 08:33:35 +0100

    add build file, update requirement and make migrations

--- reg_port_app/urls.py
+++ reg_port_app/urls.py
@@ -1,15 +1,11 @@
 from django.urls import path, include
-from .views import UserList, UserDetail, UserCreate, UserUpdate, UserDelete, UserSearchView, AdminSignUpAPIView, AdminLoginAPIView, AdminLogoutAPIView
+from .views import UserList, UserDetails, UserSearchView, AdminSignUpAPIView, AdminLoginAPIView, AdminLogoutAPIView
 
 urlpatterns = [
     path('users/', UserList.as_view()),
-    path('user-detail/<int:users_id>/', UserDetail.as_view()),
-    path('users/create/', UserCreate.as_view()),
-    path('users/<int:users_id>/update/', UserUpdate.as_view()),
-    path('users/<int:users_id>/delete/', UserDelete.as_view()),
-
+    path('user-details/', UserDetails.as_view()),
+    
     path('search/', UserSearchView.as_view({'get':'list'})),
-
    
     path('admin/signup/', AdminSignUpAPIView.as_view(), name='admin_signup'),
     path('admin/login/', AdminLoginAPIView.as_view(), name='admin_login'),
--- reg_port_app/views.py
+++ reg_port_app/views.py
@@ -7,61 +7,17 @@ from .serializers import UserSerializer, UserSearchSerializer, AdminSignUpSerial
 from rest_framework import viewsets
 from rest_framework.permissions import AllowAny
 from rest_framework.authtoken.models import Token
+from django.contrib.auth import authenticate  
+from rest_framework import generics
 
-class UserList(APIView):
+
+class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-    def get(self, request):
-        users = User.objects.all()
-        serializer = UserSerializer(users, many=True)
-        return Response(serializer.data)
-
-class UserDetail(APIView):
+class UserDetails(generics.ListCreateAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-
-    def get(self, request, users_id):
-        user = User.objects.get(id=users_id)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-
-
-class UserCreate(APIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    def post(self, request):
-        serializer = UserSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=201)
-        return Response(serializer.errors, status=400)
-
-
-class UserUpdate(APIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    def put(self, request, users_id):
-        user = User.objects.get(id=users_id)
-        serializer = UserSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=200)
-        return Response(serializer.errors, status=400)
-
-
-class UserDelete(APIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
-
-    def delete(self, request, users_id):
-        user = User.objects.get(id=users_id)
-        user.delete()
-        return Response(status=204)
-    
-
 
 class UserSearchView(viewsets.ModelViewSet):
     serializer_class = UserSearchSerializer
@@ -99,9 +55,6 @@ class AdminSignUpAPIView(APIView):
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-# admins/views.py
-from django.contrib.auth import authenticate  # Import the authenticate function
 
 class AdminLoginAPIView(APIView):
     permission_classes = [AllowAny]
